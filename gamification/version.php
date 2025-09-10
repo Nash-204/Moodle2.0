750e03e6e52fad2307428cadbdbb0d1d0bbbd8b9
Nash-204 <anusemana24@gmail.com>
2025-09-10 14:43:17 +1000

    Update version.php

--- gamification/version.php
+++ gamification/version.php
@@ -29,6 +29,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_gamification';
 $plugin->version = 20240905124; // YYYYMMDDXX.
-$plugin->requires = 2022041900; // Moodle 4.0.5 minimum (keep as you had).
+$plugin->requires = 2022041900; // Moodle 4.0.5 minimum 
 $plugin->maturity = MATURITY_STABLE;
+
 $plugin->release = '1.0';
